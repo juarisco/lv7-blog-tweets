6cc1efa54a1bf2aca66a73d2eca340a211b78294
Josef <juarisco@outlook.com>
2020-09-14 16:57:51 -0500

    3-lesson-3-controller-view-route

---
+++ app/Http/Controllers/GuestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Entry;
+use Illuminate\Http\Request;
+
+class GuestController extends Controller
+{
+    public function index()
+    {
+        return view('welcome', [
+            'entries' => Entry::all()
+        ]);
+    }
+}
